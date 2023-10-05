4feb7463f887ed44c6eed5ea55daeb848f367295
VK <vaishakh.aravind@gmail.com>
2023-10-05 22:25:06 +0530

    ui design

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -22,82 +22,90 @@ class _HomePageState extends State<HomePage> {
         centerTitle: true,
         backgroundColor: Pallete.whiteColor,
       ),
-      body: Column(
-        children: [
-          Stack(
-            children: [
-              Center(
-                child: Container(
-                  height: 120,
-                  width: 120,
-                  margin: const EdgeInsets.only(top: 4),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Center(
+                  child: Container(
+                    height: 120,
+                    width: 120,
+                    margin: const EdgeInsets.only(top: 2),
+                    decoration: const BoxDecoration(
+                      color: Pallete.assistantCircleColor,
+                      shape: BoxShape.circle,
+                      ),
+                  ),
+                ),
+                Container(
+                  height: 123,
                   decoration: const BoxDecoration(
-                    color: Pallete.assistantCircleColor,
                     shape: BoxShape.circle,
-                    ),
-                ),
-              ),
-              Container(
-                height: 123,
-                decoration: const BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(image: AssetImage('assets/images/virtualAssistant.png'))
-                ),
-              )
-            ],
-          ),
-
-          //Chat Bubble
-          Container(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 20,
-              vertical: 10,
+                    image: DecorationImage(image: AssetImage('assets/images/virtualAssistant.png'))
+                  ),
+                )
+              ],
             ),
-            margin: const EdgeInsets.symmetric(
-              horizontal: 40).copyWith(top: 30),
-
-            decoration: BoxDecoration(
-              border: Border.all(
-                color: Pallete.borderColor,
-              ),
-              borderRadius: BorderRadius.circular(20).copyWith(
-                topLeft: Radius.zero,
-              )
-            ),
-            child: const Padding(
-              padding: EdgeInsets.symmetric(
+      
+            //Chat Bubble
+            Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
                 vertical: 10,
               ),
-              child: Text('Good Morning, what can I do for you ?',
-              style: TextStyle(
+              margin: const EdgeInsets.symmetric(
+                horizontal: 30).copyWith(top: 20),
+      
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Pallete.borderColor,
+                ),
+                borderRadius: BorderRadius.circular(20).copyWith(
+                  topLeft: Radius.zero,
+                )
+              ),
+              child: const Padding(
+                padding: EdgeInsets.symmetric(
+                  vertical: 5,
+                ),
+                child: Text('Good Morning, what can I do for you ?',
+                style: TextStyle(
+                  color: Pallete.mainFontColor,
+                  fontSize: 20,
+                  fontFamily: 'Cera Pro'
+                ),),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              margin: const EdgeInsets.only(top: 5,left: 22),
+              alignment:Alignment.centerLeft ,
+              child: const Text('Here are some features :',style: TextStyle(
+                fontFamily: 'Cera Pro',
+                fontSize: 18,
                 color: Pallete.mainFontColor,
-                fontSize: 20,
-                fontFamily: 'Cera Pro'
+                fontWeight: FontWeight.bold
               ),),
             ),
-          ),
-          Container(
-            padding: const EdgeInsets.all(10),
-            margin: const EdgeInsets.only(top: 10,left: 22),
-            alignment:Alignment.centerLeft ,
-            child: const Text('Here are some features :',style: TextStyle(
-              fontFamily: 'Cera Pro',
-              fontSize: 18,
-              color: Pallete.mainFontColor,
-              fontWeight: FontWeight.bold
-            ),),
-          ),
-
-          //Features list
-          const Column(
-            children: [
-               FeatureBox(color: Pallete.firstSuggestionBoxColor,headerText: 'ChatGPT',
-               descriptionText: 'An easier way to stay connected and informed with ChatGPT',)
-            ],
-          )
-
-        ],
+      
+            //Features list
+            const Column(
+              children: [
+                 FeatureBox(color: Pallete.firstSuggestionBoxColor,headerText: 'ChatGPT',
+                 descriptionText: 'An easier way to stay connected and informed with ChatGPT',),
+                 FeatureBox(color: Pallete.secondSuggestionBoxColor,headerText: 'Dall-E',
+                 descriptionText: 'Give life to your imagination and creativity using Dall-E',),
+                 FeatureBox(color: Pallete.thirdSuggestionBoxColor,headerText: 'Smart Voice Assistant',
+                 descriptionText: 'Get the best of two worlds with voice assistant powered by Dall-E and ChatGPT',),
+              ],
+            ),
+          ],
+        ),
       ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Pallete.firstSuggestionBoxColor,
+        onPressed: (){},child: const Icon(Icons.mic),),
     );
   }
 }
--- lib/widget/feature_box.dart
+++ lib/widget/feature_box.dart
@@ -15,29 +15,36 @@ class FeatureBox extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.symmetric(
-        vertical: 10,
-        horizontal: 35
+        vertical: 5,
+        horizontal: 30
       ),
       decoration: BoxDecoration(
         color: color,
         borderRadius: const BorderRadius.all(Radius.circular(15)),
       ),
       child: Padding(
-        padding: const EdgeInsets.only(top: 20, left: 15,bottom: 20),
+        padding: const EdgeInsets.only(top: 15, left: 15,bottom: 20),
         child: Column(
           children: [
-            Text(headerText,style: const TextStyle(
-              fontFamily: 'Cera Pro',
-              color: Pallete.blackColor,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+            Container(
+              alignment: Alignment.centerLeft,
+              child: Text(headerText,style: const TextStyle(
+                fontFamily: 'Cera Pro',
+                color: Pallete.blackColor,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
             ),
-            ),
-            Text(descriptionText,style: const TextStyle(
-              fontFamily: 'Cera Pro',
-              color: Pallete.blackColor,
-              fontSize: 14,
-            ),
+            const SizedBox(height: 2),
+            Padding(
+              padding: const EdgeInsets.only(right:10.0),
+              child: Text(descriptionText,style: const TextStyle(
+                fontFamily: 'Cera Pro',
+                color: Pallete.blackColor,
+                fontSize: 14,
+              ),
+              ),
             ),
           ],
         ),
